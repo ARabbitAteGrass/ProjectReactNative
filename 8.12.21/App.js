ebeb88673de65f16329333d474917f10c7d86439
MookieS <50792459+ARabbitAteGrass@users.noreply.github.com>
2021-12-08 12:14:15 +0700

    week5

---
+++ 8.12.21/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState, useMemo } from 'react';
+import ImageWithTextInput from './components/ImageWithTextInput';
+import ButtonExample from './components/ButtonExample';
+import TextInputPractice1 from './components/TextInputPractice1'
+import TouchableExample from './components/TouchableExample';
+import TouchablePractice from './components/TouchablePractice';
+
+const App = () => {
+  
+    return (
+      //<ImageWithTextInput/>
+      //<ButtonExample/>
+      //<TextInputPractice1/>
+      //<TouchableExample/>
+      <TouchablePractice/>
+    );
+  };
+
+  export default App;
